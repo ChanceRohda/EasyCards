25e8a94ff6c36d5192c7e976f6b5471049509f64
“ChanceRohda” <chancerohda@gmail.com>
2023-06-26 13:25:48 -0500

    First table view in progress. Not functional yet.

--- EasyCards/ViewController.swift
+++ EasyCards/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var setName: UILabel!
+    @IBOutlet weak var setListTableView: UITableView!
+    var setNames : [String] = ["Set 1"]
+    let cellReuseIdentifier = "setCell"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setListTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        setListTableView.delegate = self
+        setListTableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return setNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = (self.setListTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return
+    }
+    
+    @IBAction func didTapEditButton(_ sender: Any) {
+    }
+    
+    @IBAction func didTapSelectButton(_ sender: Any) {
+    }
 }
 
